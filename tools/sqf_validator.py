7be408bf442782be53a28e82a1c8191d4bc8aff5
Jacob Oxley <jdoxley6@gmail.com>
2019-11-05 19:35:47 -0800

    11/5/19

--- tools/sqf_validator.py
+++ tools/sqf_validator.py
@@ -168,7 +168,7 @@ def main():
     args = parser.parse_args()
 
     # Allow running from root directory as well as from inside the tools directory
-    rootDir = "funtions/"
+    rootDir = "functions/"
     if (os.path.exists("addons")):
         rootDir = "addons"
 
